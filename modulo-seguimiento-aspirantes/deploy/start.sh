254b374811e7af76313bc12b1694edf5a4f32bea
velasco-coder <a20240094@utem.edu.mx>
2026-05-22 12:07:12 -0600

    Ajustar Render para plan gratis

--- modulo-seguimiento-aspirantes/deploy/start.sh
+++ modulo-seguimiento-aspirantes/deploy/start.sh
@@ -7,6 +7,9 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
+php artisan migrate --force
+php artisan db:seed --force
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
